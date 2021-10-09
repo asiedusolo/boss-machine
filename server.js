1570a95321e68f456cbe3e77891e9eee6e08eb55
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-09 09:51:01 -0400

    adding bodyParser and cors middleware

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+const cors = require('cors')
+app.use(cors())
 
 // Add middware for parsing request bodies here:
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter)
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
