bd14c1bb3860dbc944dde843d5fd21db777c6baa
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-09 12:37:01 -0400

    implementing get minion by minionId route

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(cors())
 
 // Add middware for parsing request bodies here:
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(express.json({extended: false}));
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
--- server/minionsRoute.js
+++ server/minionsRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const minionsRouter = express.Router()
-const {getAllFromDatabase} = require('./db')
+const {getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase} = require('./db')
 
 minionsRouter.get('/', (req, res, next) => {
     const minionsArray = getAllFromDatabase('minions')
@@ -8,6 +8,19 @@ minionsRouter.get('/', (req, res, next) => {
         minions: minionsArray
     })
 })
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    const minionId = req.params.minionId
+    const minion = getFromDatabaseById('minions', minionId)
+    if(minion){
+        res.send({
+            minion: minion
+        })
+    }else{
+        res.status(404).send()
+    }
+})
+
 
 
 module.exports = minionsRouter
