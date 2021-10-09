044d1d9dcfa631774b9358a0cc2321ce4da96ea9
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-09 12:37:43 -0400

    implementing post minion route

--- server/minionsRoute.js
+++ server/minionsRoute.js
@@ -21,6 +21,24 @@ minionsRouter.get('/:minionId', (req, res, next) => {
     }
 })
 
+minionsRouter.post('/', (req, res, next) => {
+    console.log('Got Body: ', req.body)
+    const name = req.body.name
+    const title = req.body.title
+    const salary = req.body.salary
+    const weaknesses = req.body.weaknesses
+    if(name === "" || title === "" || weaknesses === "" || salary === ""){
+        res.status(400).send()
+    }
+    else{
+        req.body.salary = Number(salary)
+        const newMinion = addToDatabase('minions', req.body)
+        res.status(201).send({
+            minion: newMinion
+        })
+    }
+})
+
 
 
 module.exports = minionsRouter
