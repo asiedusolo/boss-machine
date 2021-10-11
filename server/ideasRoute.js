c4760e15e84f0cd5c52035a30c720587c7d073d4
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-11 08:17:45 -0400

    implementing post idea route

--- server/ideasRoute.js
+++ server/ideasRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ideasRouter = express.Router()
-const {getAllFromDatabase, getFromDatabaseById} = require('./db.js')
+const {getAllFromDatabase, getFromDatabaseById, addToDatabase} = require('./db.js')
 
 ideasRouter.get('/', (req, res, next) => {
     const ideasArray = getAllFromDatabase('ideas')
@@ -18,6 +18,25 @@ ideasRouter.get('/:ideaId', (req, res, next) => {
         })
     } else{
         res.status(404).send()
+    }
+})
+
+ideasRouter.post('/', (req, res, next) => {
+    console.log('Got Body: ', req.body)
+    const name = req.body.name
+    const description = req.body.description
+    const weeklyRevenue = req.body.weeklyRevenue
+    const numWeeks = req.body.numWeeks
+    if(name === "" || description === "" || weeklyRevenue === "" || numWeeks === ""){
+        res.status(400).send()
+    }
+    else{
+        req.body.weeklyRevenue = Number(weeklyRevenue)
+        req.body.numWeeks = Number(numWeeks)
+        const newIdea = addToDatabase('ideas', req.body)
+        res.status(201).send({
+            idea: newIdea
+        })
     }
 })
 
