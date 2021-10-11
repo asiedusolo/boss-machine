1a1edd82a6b7417f6555a51c2a5543be7505076f
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-11 09:50:18 -0400

    updating post meetings route to use createMeeting function

--- server/meetingsRoute.js
+++ server/meetingsRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const meetingsRouter = express.Router()
 
-const {getAllFromDatabase, addToDatabase, deleteAllFromDatabase} = require('./db')
+const {getAllFromDatabase, addToDatabase, deleteAllFromDatabase, createMeeting} = require('./db')
 
 meetingsRouter.get('/', (req, res, next) => {
     const meetingsArray = getAllFromDatabase('meetings')
@@ -11,21 +11,13 @@ meetingsRouter.get('/', (req, res, next) => {
 })
 
 meetingsRouter.post('/', (req, res, next) => {
-    console.log('Got Body: ', req.body)
-    const time = req.body.time
-    const date = req.body.date
-    const day = req.body.day
-    const note = req.body.note
-    if(time === "" || day === "" || note === ""){
-        res.status(400).send()
-    }
-    else{
-        req.body.date = new Date(req.body.day).toISOString()
-        req.body.day = new Date(req.body.day).toString().slice(0,15)
-        const newMeeting = addToDatabase('meetings', req.body)
+    const newMeeting = addToDatabase('meetings', createMeeting())
+    if(newMeeting){
         res.status(201).send({
             meeting: newMeeting
         })
+    }else{
+        res.status(400).send()
     }
 })
 
