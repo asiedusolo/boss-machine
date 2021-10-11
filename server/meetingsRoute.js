594481a2481badea24c4c12456982a77c89c0c2d
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-11 09:17:08 -0400

    implementing post meeting route

--- server/meetingsRoute.js
+++ server/meetingsRoute.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const meetingsRouter = express.Router()
 
-const {getAllFromDatabase} = require('./db')
+const {getAllFromDatabase, addToDatabase} = require('./db')
 
 meetingsRouter.get('/', (req, res, next) => {
     const meetingsArray = getAllFromDatabase('meetings')
     res.send({
         meetings: meetingsArray
     })
+})
+
+meetingsRouter.post('/', (req, res, next) => {
+    console.log('Got Body: ', req.body)
+    const time = req.body.time
+    const date = req.body.date
+    const day = req.body.day
+    const note = req.body.note
+    if(time === "" || day === "" || note === ""){
+        res.status(400).send()
+    }
+    else{
+        req.body.date = new Date(req.body.day).toISOString()
+        req.body.day = new Date(req.body.day).toString().slice(0,15)
+        const newMeeting = addToDatabase('meetings', req.body)
+        res.status(201).send({
+            meeting: newMeeting
+        })
+    }
 })
 
 
