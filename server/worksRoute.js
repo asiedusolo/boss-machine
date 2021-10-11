b5cd2a7c9c489d272280563ae55a8fe70d394759
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-11 11:32:05 -0400

    implementing post work on minionId

--- server/worksRoute.js
+++ server/worksRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getFromDatabaseById, getAllFromDatabase } = require('./db')
+const { getFromDatabaseById, getAllFromDatabase, addToDatabase } = require('./db')
 const worksRouter = express.Router({mergeParams: true})
 
 worksRouter.get('/', (req, res, next) => {
@@ -19,7 +19,23 @@ worksRouter.get('/', (req, res, next) => {
 })
 
 worksRouter.post('/', (req, res, next) => {
-    
+    const title = req.body.title
+    const description = req.body.description
+    const hours = req.body.hours
+    if(title === "" || description === "" || hours === ""){
+        res.status(404).send()
+    }else{
+        req.body.minionId = req.params.minionId
+        req.body.hours = Number(req.body.hours)
+        const newWork = addToDatabase('work', req.body)
+        if(newWork){
+            res.send({
+                work: newWork
+            })
+        }else{
+            res.status(404).send()
+        }
+    }
 })
 
 
